fdfe11e941e4456b53df57e0132031c5f486404e
Ben2104 <dohoangkhoi341@gmail.com>
2024-06-20 15:11:30 -0700

    add Customer.py

--- Final/Customer.py
+++ Final/Customer.py
@@ -4,3 +4,13 @@ class Customer:
 
     def get_tax_rate(self):
         return 0.09  
+    
+    def applyTax(total_price, customer):
+        """Apply tax based on user type."""
+        if customer.isStudent():
+            return total_price
+        elif not customer.isStudent():
+            tax = total_price * 0.09
+            return total_price + tax
+        else:
+            return "Invalid user type."
