1cd0549239d2aed3f7277c2f9f82dd78c7d1952b
Ben2104 <dohoangkhoi341@gmail.com>
2024-06-21 11:09:47 -0700

    updates Customer.py by adding isStudent function

--- Final/Customer.py
+++ Final/Customer.py
@@ -7,3 +7,8 @@ class Customer:
 
     def applyTax(self, amount):
         raise NotImplemented
+    def isStudent(self, user_type):
+        if (user_type == "student"):
+            return True
+        else:
+            return False
