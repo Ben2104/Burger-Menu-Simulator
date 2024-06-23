80264ab9c17aeeff3352e5d040211818d1de3080
Ben2104 <dohoangkhoi341@gmail.com>
2024-06-23 16:15:35 -0700

    update main.py

--- Final/main.py
+++ Final/main.py
@@ -4,21 +4,34 @@ from Burger import *
 from Student import *
 from Staff import * 
 if __name__=="__main__":
-    o = Order()
-    o.displayMenu()
-    curOrder = o.getOrder()
-    curPrice = o.calculatePrice(curOrder)
-    student = Student()
-    staff = Staff()
-    
-    customer = Customer()
-    user_type = input("Are you a student or a staff? ").strip().lower()
-    
-    if (customer.isStudent(user_type)):
-        customer_type = Student()
-    else:
-        customer_type = Staff()
-    
-    bill = o.display_bill(curOrder, curPrice, customer_type)
-    o.saveToFile(bill)
-    print(bill)
+    flag = True 
+
+    while flag:
+        o = Order()
+        o.displayMenu()
+        curOrder = o.getOrder()
+        if (curOrder == False):
+            break
+        curPrice = o.calculatePrice(curOrder)
+        student = Student()
+        staff = Staff()
+        
+        customer = Customer()
+        user_type = input("Are you a student or a staff? ").strip().lower()
+        
+        if (customer.isStudent(user_type)):
+            customer_type = Student()
+        else:
+            customer_type = Staff()
+        
+        bill = o.display_bill(curOrder, curPrice, customer_type)
+        print(bill)
+        
+        userInputToContinue = input("Continue for another order(Any keys= Yes, n= No)?")
+
+        if userInputToContinue.lower() == 'n':
+            print("The system is shutting down!")
+            o.saveToFile(bill)
+            flag = False
+
+        
